7341e12602d5c65bcf70a141eb82b115781b28e2
Caner Sezgin <ccanersezgin@gmail.com>
2021-11-11 22:00:23 +0300

    authentication service integration

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -2,11 +2,14 @@ import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
 import commonMiddleware from '../lib/commonMiddleware';
 import HTTPError from 'http-errors';
+import validator from '@middy/validator';
+import createAuctionSchema from '../lib/schemas/createAuctionSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
   const { title } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   const now = new Date();
   const endDate = new Date();
@@ -21,6 +24,7 @@ async function createAuction(event, context) {
     highestBid: {
       amount: 0,
     },
+    seller: email,
   };
 
   try {
@@ -41,4 +45,11 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction).use(
+  validator({
+    inputSchema: createAuctionSchema,
+    ajvOptions: {
+      strict: false,
+    },
+  })
+);
--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -1,18 +1,32 @@
 import AWS from 'aws-sdk';
-import commonMiddleware from '../lib/commonMiddleware';
 import HTTPError from 'http-errors';
+import validator from '@middy/validator';
 import { getAuctionById } from './getAuction';
+import commonMiddleware from '../lib/commonMiddleware';
+import placeBidSchema from '../lib/schemas/placeBidSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   const auction = await getAuctionById(id);
 
-  if(auction.status !== 'OPEN') throw new HTTPError.Forbidden("You cannot bid on closed auctions.")
+  // Validation | Bid Identity
+  if (auction.seller === email)
+    throw new HTTPError.Forbidden('You cannot bid on your own auctions.');
 
+  // Validation | Avoid Double Bidding
+  if(auction.highestBid.bidder === email)
+    throw new HTTPError.Forbidden('You are already the highest bidder.')
+
+  // Validation | Auction Status
+  if (auction.status !== 'OPEN')
+    throw new HTTPError.Forbidden('You cannot bid on closed auctions.');
+
+  // Validation | Bid Amount
   if (amount <= auction.highestBid.amount) {
     throw new HTTPError.Forbidden(
       `Your bid must be higher than ${auction.highestBid.amount}!`
@@ -22,9 +36,11 @@ async function placeBid(event, context) {
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: 'set highestBid.amount = :amount',
+    UpdateExpression:
+      'set highestBid.amount = :amount, highestBid.bidder = :bidder',
     ExpressionAttributeValues: {
       ':amount': amount,
+      ':bidder': email,
     },
     ReturnValues: 'ALL_NEW',
   };
@@ -45,4 +61,11 @@ async function placeBid(event, context) {
   };
 }
 
-export const handler = commonMiddleware(placeBid);
+export const handler = commonMiddleware(placeBid).use(
+  validator({
+    inputSchema: placeBidSchema,
+    ajvOptions: {
+      strict: false,
+    },
+  })
+);
